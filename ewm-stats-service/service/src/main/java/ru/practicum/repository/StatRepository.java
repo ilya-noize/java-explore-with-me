18e80d89fdf77643f13123f807046dd2befec4aa
ilya-noize <shumakoff.ilya@yandex.ru>
2024-01-01 00:14:12 +0300

    fix repository

--- ewm-stats-service/service/src/main/java/ru/practicum/repository/StatRepository.java
+++ ewm-stats-service/service/src/main/java/ru/practicum/repository/StatRepository.java
@@ -16,11 +16,12 @@ public interface StatRepository extends JpaRepository<Stats, Long> {
      * @param uris          Список url для которых нужно выгрузить статистику
      * @return      Список всех url с количеством запросов
      */
-    @Query(value = "SELECT s.APP app, s.URI uri, COUNT(s.IP) hit" +
+    @Query(value = "SELECT s.APP app, s.URI uri, COUNT(s.IP) hits" +
             " FROM STAT s " +
             " WHERE (s.CREATED >= :createdStart AND s.CREATED <= :createdEnd)" +
             " AND s.uri IN (:uris) " +
-            " GROUP BY s.APP, s.URI",
+            " GROUP BY s.APP, s.URI " +
+            " ORDER BY hits",
             nativeQuery = true)
     List<ShowStats> getByCreatedBetweenAndUriIn(
             @Param("createdStart") LocalDateTime createdStart,
@@ -32,10 +33,11 @@ public interface StatRepository extends JpaRepository<Stats, Long> {
      * @param createdEnd    Дата и время конца диапазона за который нужно выгрузить статистику
      * @return      Список всех запросов
      */
-    @Query(value = "SELECT s.APP app, s.URI uri, COUNT(s.IP) hit" +
+    @Query(value = "SELECT s.APP app, s.URI uri, COUNT(s.IP) hits" +
             " FROM STAT as s " +
             " WHERE (created >= :createdStart AND created <= :createdEnd) " +
-            " GROUP BY s.APP, s.URI",
+            " GROUP BY s.APP, s.URI " +
+            " ORDER BY hits",
             nativeQuery = true)
     List<ShowStats> getByCreatedBetween(
             @Param("createdStart") LocalDateTime createdStart,
@@ -47,11 +49,12 @@ public interface StatRepository extends JpaRepository<Stats, Long> {
      * @param uris          Список url для которых нужно выгрузить статистику
      * @return      Список всех url с количеством уникальных запросов
      */
-    @Query(value = "SELECT s.APP app, s.URI uri, COUNT(DISTINCT s.IP) hit" +
+    @Query(value = "SELECT s.APP app, s.URI uri, COUNT(DISTINCT s.IP) hits" +
             " FROM STAT as s " +
             " WHERE (created >= :createdStart AND created <= :createdEnd)" +
             " AND uri IN (:uris) " +
-            " GROUP BY s.APP, s.URI",
+            " GROUP BY s.APP, s.URI " +
+            " ORDER BY hits",
             nativeQuery = true)
     List<ShowStats> getByCreatedBetweenAndUriInAndUniqueIp(
             @Param("createdStart") LocalDateTime createdStart,
@@ -63,10 +66,11 @@ public interface StatRepository extends JpaRepository<Stats, Long> {
      * @param createdEnd    Дата и время конца диапазона за который нужно выгрузить статистику
      * @return      Список всех уникальных запросов
      */
-    @Query(value = "SELECT s.APP app, s.URI uri, COUNT(DISTINCT s.IP) hit" +
+    @Query(value = "SELECT s.APP app, s.URI uri, COUNT(DISTINCT s.IP) hits" +
             " FROM STAT as s " +
             " WHERE (created >= :createdStart AND created <= :createdEnd) " +
-            " GROUP BY s.APP, s.URI",
+            " GROUP BY s.APP, s.URI " +
+            " ORDER BY hits",
             nativeQuery = true)
     List<ShowStats> getByCreatedBetweenAndUniqueIp(
             @Param("createdStart") LocalDateTime createdStart,
