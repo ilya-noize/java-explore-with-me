55380bf260b468d1413b73f8e54a734f49439bdb
ilya-noize <shumakoff.ilya@yandex.ru>
2024-01-23 17:45:16 +0300

    fix validation, UserController;

--- ewm-main-service/src/main/java/ru/practicum/user/api/controller/UserController.java
+++ ewm-main-service/src/main/java/ru/practicum/user/api/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.practicum.user.api.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,26 +9,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.user.api.dto.NewUserDto;
 import ru.practicum.user.api.dto.UserDto;
 import ru.practicum.user.api.service.UserService;
 
-import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import java.util.List;
 
 import static ru.practicum.constants.Constants.FROM;
 import static ru.practicum.constants.Constants.SIZE;
 
+@RestController
+@Slf4j
 @RequiredArgsConstructor
-@Validated
 public class UserController {
     private final UserService service;
 
     @PostMapping({"/admin/users"})
-    public UserDto create(@RequestBody @Valid NewUserDto newDto) {
+    public UserDto create(@RequestBody @Validated NewUserDto dto) {
+        log.debug("[i] create new user. dto: {}", dto);
 
-        return service.create(newDto);
+        return service.create(dto);
     }
 
     @GetMapping({"/admin/users"})
@@ -35,12 +38,15 @@ public class UserController {
             @RequestParam(required = false) List<Long> ids,
             @RequestParam(required = false, defaultValue = FROM) @Min(0) Integer from,
             @RequestParam(required = false, defaultValue = SIZE) @Min(1) Integer size) {
+        log.debug("[i] get all users with ids : {}. from:{}, size:{}", ids, from, size);
 
         return service.getAll(ids, from, size);
     }
 
     @DeleteMapping({"/admin/users/{id}"})
     public void remove(@PathVariable Long id) {
+        log.debug("[i] remove user with id: {}", id);
+
         service.remove(id);
     }
 }
--- ewm-main-service/src/main/java/ru/practicum/user/api/dto/NewUserDto.java
+++ ewm-main-service/src/main/java/ru/practicum/user/api/dto/NewUserDto.java
@@ -2,19 +2,18 @@ package ru.practicum.user.api.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-@Getter
-@Setter
+@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class NewUserDto {
     private @NotNull @Email String email;
-    private @NotNull String name;
+    private @NotNull @NotBlank String name;
 }
